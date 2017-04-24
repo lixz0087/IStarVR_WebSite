28cf962b3b20e75fac3470895784be7680e5433b
Surbhi050192 <surbhigo@usc.edu>
2017-04-24 00:58:27 -0700

    modified editprofile

---
+++ app/scripts/controllers/video.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name istarVrWebSiteApp.controller:FriendCtrl
+ * @description
+ * # FriendCtrl
+ * Controller of the istarVrWebSiteApp
+ */
+angular.module('istarVrWebSiteApp')
+  .controller('VideoCtrl', function () {
+
+
+
+
+
+
+  });
