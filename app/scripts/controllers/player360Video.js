418f39d6fea173ffb5d919e9b669cf78b65ae0bf
pavan987 <gondhi@usc.edu>
2017-04-26 09:33:33 -0700

    Final Submission

--- app/scripts/controllers/player360Video.js
+++ app/scripts/controllers/player360Video.js
@@ -53,7 +53,7 @@ angular.module('istarVrWebSiteApp')
 
 
     if(bucket == 'istarvr') {
-      if ($cookies.getObject("temp-s3-creds") !== undefined || $cookies.getObject("temp-s3-creds-expires-in") <= ((new Date().getTime()) - 1000)) {
+      if ($cookies.getObject("temp-s3-creds") == undefined || $cookies.getObject("temp-s3-creds-expires-in") <= ((new Date().getTime()) - 1000)) {
         requestTempS3Creds("private",function(err){
           if(!err) {
             temCredential = $cookies.getObject("temp-s3-creds");
@@ -92,7 +92,7 @@ angular.module('istarVrWebSiteApp')
 
       }
     } else {
-      if ($cookies.getObject("temp-s3-creds-public") !== undefined || $cookies.getObject("temp-s3-creds-public-expires-in") <= ((new Date().getTime()) - 1000)) {
+      if ($cookies.getObject("temp-s3-creds-public") == undefined || $cookies.getObject("temp-s3-creds-public-expires-in") <= ((new Date().getTime()) - 1000)) {
         requestTempS3Creds("public",function(err){
           if(!err) {
             temCredential = $cookies.getObject("temp-s3-creds-public");
